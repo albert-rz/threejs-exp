093a006a91c40133a55d356366e120cc2de0d03f
Albert Ruiz <ru.albert@gmail.com>
2023-01-20 08:01:32 +0100

    feat: update

--- examples/js/car.js
+++ examples/js/car.js
@@ -9,53 +9,10 @@ import {
   Vector3,
 } from 'three';
 
-export function buildPrism(
-  width = 5,
-  height = 5,
-  depth = 10,
-  color = 0x333333,
-  x = 0,
-  y = 0,
-  z = 0
-) {
-  const prism = new Mesh(
-    new BoxGeometry(width, height, depth),
-    new MeshLambertMaterial({ color: color, flatShading: true })
-  );
-  prism.position.x = x;
-  prism.position.y = y;
-  prism.position.z = z;
-
-  return prism;
-}
-
-export function buildCar(color = 0xff0000) {
-  const car = new Group();
-
-  // Car size
-  const width = 5;
-  const length = 15;
-
-  // Wheels
-  const radius = 4;
-  const pos = [
-    { x: length / 2 - 2, y: radius / 2, z: 2.5 },
-    { x: length / 2 - 2, y: radius / 2, z: -2.5 },
-    { x: -(length / 2 - 2), y: radius / 2, z: 2.5 },
-    { x: -(length / 2 - 2), y: radius / 2, z: -2.5 },
-  ];
-  for (let i in pos) {
-    console.log(pos[i]);
-    car.add(buildPrism(radius, radius, 1.5, 0x3333333, pos[i].x, pos[i].y, pos[i].z));
-  }
-
-  return car;
-}
-
-export class Prism extends Mesh {
-  constructor(width, height, depth, color, x = 0, y = 0, z = 0) {
+export class Box extends Mesh {
+  constructor(sizeX, sizeY, sizeZ, color, x = 0, y = 0, z = 0) {
     super(
-      new BoxGeometry(width, height, depth),
+      new BoxGeometry(sizeX, sizeY, sizeZ),
       new MeshLambertMaterial({ color: color, flatShading: true })
     );
 
@@ -102,17 +59,28 @@ export class Prism extends Mesh {
   }
 }
 
-export class Wheel extends Prism {
-  constructor({ radius = null, depth = null, color = 0xfafa00, x = 0, y = 0, z = 0 } = {}) {
-    super(radius, radius, depth, color, x, y, z);
+class Wheel extends Box {
+  constructor({ radius = null, thickness = null, color = 0xfafa00, x = 0, y = 0, z = 0 } = {}) {
+    super(radius, radius, thickness, color, x, y, z);
+  }
+}
+
+class Body extends Box {
+  constructor({width = null, length = null, height = null, color = 0xff0000, x = 0, y = 0, z = 0}) {
+    // super(width, )
+    super(length, height, width, color, x, y, z);
   }
 }
 
 export class Car extends Group {
-  constructor(width, height, lenght, color) {
+  constructor(length, width, height, color) {
     super();
 
+    const radius = 4
+    const thick = 2
+
     // Wheels
-    this.add(new Wheel({ radius: 5, depth: 2 }));
+    this.add(new Wheel({ radius: radius, thickness: thick, y: radius/2, z: width/2 - thick/2 + 0.2}));
+    this.add(new Body({width: width, length: length, height: height, y: height/2 + thick/2}))
   }
 }
